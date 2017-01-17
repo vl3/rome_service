79c3031eb50c4cc25b65c6734fef7445472ba8da
Valentín Sanjuan <valesanjuan9@gmail.com>
2017-01-17 14:38:27 -0300

    Added jbuilder, routes for services and remove fixturing

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'roman_numerals/convert_decimal', defaults: {format: :json}
+  get 'roman_numerals/convert_roman', defaults: {format: :json}
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
